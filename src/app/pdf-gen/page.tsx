04f78fc4e0e347e4940db86be02225a7e0757659
Elias Emon <eliasemon0025@gmail.com>
2024-04-17 19:17:21 +0600

    fix: header and the footer is moved to the layout component.

---
+++ src/app/pdf-gen/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const PdfGen: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default PdfGen
